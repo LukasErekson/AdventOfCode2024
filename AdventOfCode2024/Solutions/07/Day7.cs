68cfcd96111a2fc0727571c91bcb72bac2b7ad0f
Lukas <Lukas.Erekson@gmail.com>
2024-12-07 20:51:35 -0700

    Day7: Implement Day 7 part 2

--- AdventOfCode2024/Solutions/07/Day7.cs
+++ AdventOfCode2024/Solutions/07/Day7.cs
@@ -3,7 +3,8 @@ namespace AdventOfCode2024.Solutions;
 public class Day7 : IDay
 {
     private string _inputFilePath;
-    private long _sumOfPart1;
+    private long _sumWithAddAndMultiply;
+    private long _sumWithConcatenate;
 
     public Day7(string inputFilePath)
     {
@@ -13,12 +14,12 @@ public class Day7 : IDay
 
     public string PartOne()
     {
-        return $"The sum of valid questions is: {_sumOfPart1}.";
+        return $"The sum of valid questions is with add and multiply is: {_sumWithAddAndMultiply}.";
     }
 
     public string PartTwo()
     {
-        throw new NotImplementedException();
+        return $"The sum of valid equations with add, multiply, and concatinate is: {_sumWithAddAndMultiply + _sumWithConcatenate}";
     }
 
     public void ProcessInput()
@@ -28,24 +29,35 @@ public class Day7 : IDay
             using var streamReader = new StreamReader(_inputFilePath);
             string? line;
 
+            static long multiply(long x, long y) => x * y;
+            static long add(long x, long y) => x + y;
+            static long concatenate(long x, long y) => long.Parse(x.ToString() + y.ToString());
+
+            List<Func<long, long, long>> part1List = [multiply, add];
+            List<Func<long, long, long>> part21List = [concatenate, multiply, add];
+
+            var operators = new List<Func<long, long, long>> { (x, y) => x * y, (x, y) => x + y };
+
             while ((line = streamReader.ReadLine()) != null)
             {
                 var resultAndOperands = line.Split(": ");
                 var result = long.Parse(resultAndOperands[0]);
                 var operands = resultAndOperands[1].Split(' ').Select(long.Parse);
 
-                if (ValidEquationWithAddOrMultiply(result, operands))
+                if (ValidEquationWithWithOperators(result, operands, part1List))
                 {
-                    _sumOfPart1 += result;
+                    _sumWithAddAndMultiply += result;
+                }
+                else if (ValidEquationWithWithOperators(result, operands, part21List))
+                {
+                    _sumWithConcatenate += result;
                 }
             }
         }
     }
 
-    private bool ValidEquationWithAddOrMultiply(long testResult, IEnumerable<long> operands)
+    private bool ValidEquationWithWithOperators(long testResult, IEnumerable<long> operands, IEnumerable<Func<long, long, long>> operators)
     {
-        var operators = new List<Func<long, long, long>> { (x, y) => x * y, (x, y) => x + y };
-
         var operations = operands.Count() - 1;
 
         foreach (var permutation in GetPermutationsWithRept(operators, operations))
@@ -75,9 +87,11 @@ public class Day7 : IDay
     // Code from Pengyang, https://stackoverflow.com/questions/1952153/what-is-the-best-way-to-find-all-combinations-of-items-in-an-array
     static IEnumerable<IEnumerable<T>> GetPermutationsWithRept<T>(IEnumerable<T> list, int length)
     {
-        if (length == 1) return list.Select(t => new T[] { t });
-        return GetPermutationsWithRept(list, length - 1)
-            .SelectMany(t => list,
-                (t1, t2) => t1.Concat(new T[] { t2 }));
+        if (length == 1)
+        {
+            return list.Select(t => new T[] { t });
+        }
+
+        return GetPermutationsWithRept(list, length - 1).SelectMany(t => list, (t1, t2) => t1.Concat([t2]));
     }
 }
